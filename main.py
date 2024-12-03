a7f3abdd6667a6c96827a20ce04bb62cdddf67b3
Aydario <aydar.zh@mail.ru>
2024-12-03 18:24:44 +0300

    corrected version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import re
 
 # Список ключевых слов для поиска
 KEYWORDS = ['дизайн', 'фото', 'web', 'python']
@@ -8,17 +9,19 @@ KEYWORDS = ['дизайн', 'фото', 'web', 'python']
 
 def fetch_articles(url):
     response = requests.get(url)
-    return BeautifulSoup(response.text, features='lxml')
+    if response.status_code == 200:
+        return BeautifulSoup(response.text, features='lxml')
+    return response.status_code
 
 
 def parse_article(article_soup):
-    header = article_soup.select_one('h1.tm-title').find('span').text.strip()
-    author = article_soup.select_one('a.tm-user-info__username').text.strip()
+    header = article_soup.select_one('h1.tm-title').find('span')
+    author = article_soup.select_one('a.tm-user-info__username')
     time = article_soup.select_one('time')['datetime']
-    text = article_soup.select_one('div.article-formatted-body').get_text(separator='\n', strip=True).lower()
+    text = article_soup.select_one('div.article-formatted-body').get_text(separator='\n', strip=True)
     return {
-        'title': header,
-        'author': author,
+        'title': header.text.strip() if header else 'Заголовок отсутствует',
+        'author': author.text.strip() if author else 'Неизвестный автор',
         'time': time,
         'text': text
     }
@@ -27,37 +30,42 @@ def parse_article(article_soup):
 def main():
     # Получаем список статей с главной страницы
     soup = fetch_articles('https://habr.com/ru/articles')
-    articles_list = soup.select_one('div.tm-articles-list')
-    articles = articles_list.select('article.tm-articles-list__item')
+    if isinstance(soup, BeautifulSoup):
+        articles_list = soup.select_one('div.tm-articles-list')
+        articles = articles_list.select('article.tm-articles-list__item')
 
-    parsed_data = []
-    for article in articles:
-        link = 'https://habr.com' + article.select_one('a.tm-title__link')['href']
-        article_soup = fetch_articles(link)
+        parsed_data = []
+        for article in articles:
+            link = 'https://habr.com' + article.select_one('a.tm-title__link')['href']
+            article_soup = fetch_articles(link)
+            if isinstance(article_soup, BeautifulSoup):
+                article_data = parse_article(article_soup)
+                for word in KEYWORDS:
+                    if re.search(f'{word}', article_data['text'], re.IGNORECASE):
+                        parsed_data.append({
+                            'link': link,
+                            'title': article_data['title'],
+                            'author': article_data['author'],
+                            'time': article_data['time'],
+                        })
+                        print(
+                            f'{len(parsed_data)} article:\n'
+                            f'link: {link}\n'
+                            f'title: {article_data["title"]}\n'
+                            f'author: {article_data["author"]}\n'
+                            f'time: {article_data["time"]}\n\n'
+                        )
+                        break
+            else:
+                print(f'Error! Response status code: {article_soup}')
 
-        article_data = parse_article(article_soup)
-        for word in KEYWORDS:
-            if word in article_data['text']:
-                parsed_data.append({
-                    'link': link,
-                    'title': article_data['title'],
-                    'author': article_data['author'],
-                    'time': article_data['time'],
-                })
-                print(
-                    f'{len(parsed_data)} article:\n'
-                    f'link: {link}\n'
-                    f'title: {article_data["title"]}\n'
-                    f'author: {article_data["author"]}\n'
-                    f'time: {article_data["time"]}\n\n'
-                )
-                break
+        print(f'A total of {len(parsed_data)} articles were found with these keywords')
 
-    print(f'A total of {len(parsed_data)} articles were found with these keywords')
-
-    # Сохраняем результаты в JSON-файл
-    with open('articles.json', 'w', encoding='utf-8') as f:
-        f.write(json.dumps(parsed_data, ensure_ascii=False, indent=4))
+        # Сохраняем результаты в JSON-файл
+        with open('articles.json', 'w', encoding='utf-8') as f:
+            f.write(json.dumps(parsed_data, ensure_ascii=False, indent=4))
+    else:
+        print(f'Error! Response status code: {soup}')
 
 
 if __name__ == '__main__':
---
+++ version_with_aiohttp.py
@@ -0,0 +1,84 @@
+import aiohttp
+import asyncio
+import json
+import logging
+from bs4 import BeautifulSoup
+import re
+
+# Настройка логгирования
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger(__name__)
+
+# Список ключевых слов для поиска
+KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+
+async def fetch_articles(session, url):
+    async with session.get(url) as response:
+        if response.status == 200:
+            response_text = await response.text()
+            return BeautifulSoup(response_text, features='lxml')
+        else:
+            logger.error(f"Failed to fetch {url}: {response.status}")
+            return None
+
+def parse_article(article_soup):
+    header = article_soup.select_one('h1.tm-title').find('span')
+    author = article_soup.select_one('a.tm-user-info__username')
+    time = article_soup.select_one('time')['datetime']
+    text = article_soup.select_one('div.article-formatted-body').get_text(separator='\n', strip=True)
+    return {
+        'title': header.text.strip() if header else 'Заголовок отсутствует',
+        'author': author.text.strip() if author else 'Неизвестный автор',
+        'time': time,
+        'text': text
+    }
+
+async def process_article(session, article, parsed_data):
+    link = 'https://habr.com' + article.select_one('a.tm-title__link')['href']
+    article_soup = await fetch_articles(session, link)
+    
+    if article_soup:
+        article_data = parse_article(article_soup)
+        for word in KEYWORDS:
+            if re.search(f'{word}', article_data['text'], re.IGNORECASE):
+                parsed_data.append({
+                    'found keyword': word,
+                    'link': link,
+                    'title': article_data['title'],
+                    'author': article_data['author'],
+                    'time': article_data['time'],
+                })
+                logger.info(
+                    f'{len(parsed_data)} article:\n'
+                    f'found keyword "{word}" in article\n'
+                    f'link: {link}\n'
+                    f'title: {article_data["title"]}\n'
+                    f'author: {article_data["author"]}\n'
+                    f'time: {article_data["time"]}\n\n'
+                    )
+                break
+    else:
+        logger.error(f"Failed to process article: {link}")
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        # Получаем список статей с главной страницы
+        soup = await fetch_articles(session, 'https://habr.com/ru/articles')
+        if soup:
+            articles_list = soup.select_one('div.tm-articles-list')
+            articles = articles_list.select('article.tm-articles-list__item')
+
+            parsed_data = []
+            tasks = [process_article(session, article, parsed_data) for article in articles]
+            await asyncio.gather(*tasks)
+
+            logger.info(f'A total of {len(parsed_data)} articles were found with these keywords')
+
+            # Сохраняем результаты в JSON-файл
+            with open('articles_with_aiohttp.json', 'w', encoding='utf-8') as f:
+                f.write(json.dumps(parsed_data, ensure_ascii=False, indent=4))
+        else:
+            logger.error('Failed to fetch main page')
+
+# Запуск асинхронной функции
+asyncio.run(main())
